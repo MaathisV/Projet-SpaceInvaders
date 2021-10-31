87b59483f295d4b85efe5145f5fb7ad969acc82f
MaathisV <67432530+MaathisV@users.noreply.github.com>
2021-10-31 10:52:40 +0000

    Affichage et aiguillage du menu + fonction DeplacementVaisseau

--- main.c
+++ main.c
@@ -1,101 +1,19 @@
-/*
+/*      PROJET SPACE INVADERS
+    BERGEREAU Paul et VADEZ Mathis
+Développement d'un jeu type Space Invaders en language C et directement dans le terminal
+Pour executer le programme dans la console repl.it
+gcc -o projet main.c -lncurses
+./projet*/
 
- */
 
 #include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-/*-------------------------------------------------------------*/
-/* DECLARATIONS DES FCTS DE GESTION DU TERMINAL */
-/*-------------------------------------------------------------*/
-void color(int cT, int cF);
-char nb_getch();
-char b_getch();
-void startscr();
 
-/*-------------------------------------------------------------*/
-/* DECLARATION DES FCTS DE GESTION DU JEU */
-/*-------------------------------------------------------------*/
-
-
-
-
-
-/*-------------------------------------------------------------*/
-/* FONCTION PRINCIPALE */
-/*-------------------------------------------------------------*/
-int main()
-{
-    // Déclaration des variables
-    int a=1;
-    int x_vaisseau; //abscisse vaisseau
-    int xn_vaisseau;  //abscisse n-1 du vaisseau
-    int clavier; //action clavier utilisateur
-
-    // Les instructions avant le début du jeu
-    startscr();
-
-    // Affichage de l'écran d'accueil et du menu
-    printw("Bienvenue dans le jeu\n que voulez vous faire\n1 - Jouer\n2 - Quitter\n Votre choix :");
-    scanw("%d", &a);
-    refresh();
-
-
-    // Dans la boucle while les instruction pour le jeu
-    while(a==1){
-
-      clavier = nb_getch();
-
-      //affichage vaisseau
-  
-
-        // Modification des données du jeu et entrees utilisateur
-        if (clavier=='q')
-        {
-          xn_vaisseau = x_vaisseau-3;
-          move(0,xn_vaisseau);
-          printw("   ");
-          x_vaisseau = x_vaisseau - 1;
-          move(0,x_vaisseau);
-          printw("<|>");
-        }
-        else if (clavier=='d')
-        {
-          xn_vaisseau = x_vaisseau-3;
-          move(0,xn_vaisseau);
-          printw("   ");
-          x_vaisseau = x_vaisseau + 1;
-          move(0,x_vaisseau);
-          printw("<|>");
-        }
-
-        else if (clavier=='p')
-        {
-          system("clear");
-        }
-
-        refresh();
-        sleep(0.01);
-    }
-
-
-    // Les instructions avant la cloture du programme
-    endwin();
-
-    return 0;
-}
-
-
-
-
-/*-------------------------------------------------------------*/
-/* DEFINITIONS DES FCTS DE GESTION DU TERMINAL */
-/*-------------------------------------------------------------*/
-
-/************************************************************/
-/*  Choix des couleurs de la police et de la fen�tre CONSOLE
+//FONCTION DE MISE EN FORME DU TERMINAL
+/*Choix des couleurs de la police et de la fenêtre CONSOLE
     cT correspond a la couleur du texte
     cF correspond a la couleur de fond
     Codes couleus :
@@ -115,47 +33,137 @@ int main()
         13: rose fluo
         14: jaune fluo
         15: blanc               */
-/************************************************************/
 void color(int cT,int cF)
 {
     init_pair(1,cT,cF);
     attron(COLOR_PAIR(1));
 }
 
-/*-----------------------------------------------------------*/
-/* Retourne la touche pressee sans bloquer le deroulement    */
-/*-----------------------------------------------------------*/
+
+
+/*Fonction qui récupère la touche pressée sans mettre en pause l'exécution
+Entrées : néant
+Sorties : néant*/
 char nb_getch()
 {
     timeout(0);
     return getch();
 }
 
-/*-----------------------------------------------------------*/
-/* Retourne la touche pressee en bloquant le deroulement     */
-/*-----------------------------------------------------------*/
+
+
+/*Fonction qui récupère la touche pressée en mettant en pause l'exécution
+Entrées : néant
+Sorties : néant*/
 char b_getch()
 {
     timeout(-1);
     return getch();
 }
 
-/*-----------------------------------------------------------*/
-/* Initialise l'ecran */
-/*-----------------------------------------------------------*/
+
+
+/*Fonction d'initialisation de l'écran
+Entrées : néant
+Sorties : néant*/
 void startscr()
 {
     initscr();
     start_color();
     cbreak();
     noecho();
-    //curs_set(0);
+    curs_set(0);
     keypad(stdscr, true);
 }
 
 
-/*-------------------------------------------------------------*/
-/* DEFINITION DES FCTS DE GESTION DU JEU */
-/*-------------------------------------------------------------*/
+
+/*FONCTION ChoixMenu() : Affiche un menu et récupère le choix utilisateur 
+Entrées : néant
+Sorties : choix (choix de l'utilisateur ; (1:Jouer, 2: Règles, 3:Paramètres, 4:Scores, 5:Quitter))*/
+int ChoixMenu()
+{
+    int choix;  //Valeur du choix utilisateur
+    // Affichage de l'écran d'accueil et du menu
+    printw("Bienvenue dans le jeu\n que voulez vous faire\n1 - Jouer\n2 - Règles\n3 - Paramètres\n4 - Scores\n5 - Quitter\n Votre choix :");
+    scanw("%d", &choix);
+    refresh();
+    return choix;
+}
 
 
+
+/*FONCTION DeplacementVaisseau() : Recupère l'entrée clavier et fait bouger le vaisseau sur la console
+Entrées : néant
+Sorties : néant*/
+int DeplacementVaisseau()
+{
+        // Déclaration des variables
+    int x_vaisseau; //abscisse vaisseau
+    int xn_vaisseau;  //abscisse n-1 du vaisseau
+    unsigned char clavier=0; //action clavier utilisateur
+
+
+    // Dans la boucle while les instruction pour le jeu
+    while(clavier=='p')
+    {
+        clavier = nb_getch();
+
+        //affichage vaisseau
+  
+
+        // Modification des données du jeu et entrees utilisateur
+        if (clavier=='q')
+        {
+            xn_vaisseau = x_vaisseau-3;
+            move(0,xn_vaisseau);
+            printw("   ");
+            x_vaisseau = x_vaisseau - 1;
+            move(0,x_vaisseau);
+            printw("<|>");
+        }
+        else if (clavier=='d')
+        {
+            xn_vaisseau = x_vaisseau-3;
+            move(0,xn_vaisseau);
+            printw("   ");
+            x_vaisseau = x_vaisseau + 1;
+            move(0,x_vaisseau);
+            printw("<|>");
+        }
+
+        else if (clavier=='p')
+        {
+            system("clear");
+        }
+
+        refresh();
+        sleep(0.01);
+    }
+}
+
+
+
+int main()
+{
+    startscr();
+
+        //Affichage du menu et aiguillage
+    ChoixMenu();
+    switch(ChoixMenu())
+    {
+        case 1: printw("Jouer");
+        break;
+        case 2: printw("Règles");
+        break;
+        case 3: printw("Paramètres");
+        break;
+        case 4: printw("Scores");
+        break;
+        case 5: printw("Quitter");
+    }
+
+    endwin();
+
+    return 0;
+}
